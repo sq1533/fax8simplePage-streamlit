051cd9f3bbddc80b6c77e6e1bc9b3c7d623e4c0b
sq1533 <618356@naver.com>
2024-11-29 16:13:34 +0900

    feat : HTML 생성기능_ver0.4

--- home.py
+++ home.py
@@ -1,75 +1,123 @@
 import streamlit as st
 from jinja2 import Template
 import pandas as pd
-
+import os
+from playwright.sync_api import sync_playwright
+#html to image
+def toImage(htmlPath,imagePath):
+    with sync_playwright() as p:
+        # Chromium 브라우저 실행
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(viewport={"width":850, "height":1200})
+        page = context.new_page()
+        #절대 경로 변환, 'file://' 프로토콜
+        absolute_path = os.path.abspath(htmlPath)
+        file_url = f"file://{absolute_path}"
+        # HTML 파일 로드
+        page.goto(file_url)
+        # 페이지 스크린샷 저장
+        page.screenshot(path=imagePath,full_page=True)
+        browser.close()
 htmlForm = "C:\\Users\\USER\\ve_1\\samplePage\\htmlForm\\{servise}.html"
+#formating 작업
+def formating(form,sec_1_bank,sec_1_acount,sec_1_name,sec_1_time,sec_1_cost,sec_2_bank,sec_2_acount,sec_2_time,sec_3_bank,sec_3_acount,tradeNo,orderNo,antherInfo,send):
+    #jinja templete 변경 및 formating
+    fax8 = Template(form).render(
+        section_1_bank = sec_1_bank, #재이전 모계좌 은행
+        section_1_acount = sec_1_acount, #재이전 모계좌 번호
+        section_1_name = sec_1_name, #재이전 가맹점
+        section_1_time = sec_1_time, #정산 시간
+        section_1_cost = sec_1_cost, #피해금
+        section_2_bank = sec_2_bank, #입금 은행
+        section_2_acount = sec_2_acount, #입금 계좌
+        section_2_time = sec_2_time, #입금 시간
+        section_3_bank = sec_3_bank, #정산 계좌 은행
+        section_3_acount = sec_3_acount, #정산 계좌 번호
+        trNum = tradeNo, #거래번호
+        orNum = orderNo, #주문번호
+        otherInfomation = antherInfo, #특이사항
+        sendBank = send, #수신 은행
+        today = "fix" #발신 날짜
+    )
+    return fax8
 #선불 및 비선불 선택
 option1,option2 = st.tabs(["선불","비선불"])
 with option1:
     #선불가맹점 서비스
-    servise = st.radio(label="서비스",options=["간편결제(내통장결제 포함)","VAN가상계좌","PG가상계좌","010PAY"])
+    servise = st.selectbox(label="서비스",options=["간편결제(내통장결제 포함)","VAN가상계좌","PG가상계좌","010PAY"])
     #간편,내통장
     if servise == "간편결제(내통장결제 포함)":
-        st.write("1.재이전된 계좌 정보 입력")
+        st.write("### 1.재이전된 계좌 정보 입력")
         mid = st.selectbox(label="MID",options=["smile_r","M2060628"],index=None,placeholder="선택")
-        section_1_bankIndex,section_1_bank,section_1_acountIndex,section_1_acount = st.columns(spec=[1,1,1,3],vertical_alignment="center")
-        section_1_nameIndex,section_1_name,empty = st.columns(spec=[1,2,3],vertical_alignment="center")
+        section_1_bankIndex,section_1_bank,section_1_acountIndex,section_1_acount = st.columns(spec=[1,1,1,3],gap="small",vertical_alignment="center")
+        section_1_nameIndex,section_1_name,empty = st.columns(spec=[1,2,3],gap="small",vertical_alignment="center")
         if mid == None:
             section_1_bankIndex.write("은행 : ")
-            section_1_bank.text_input(label="재이전계좌 은행",value=None,label_visibility="collapsed")
+            bank1 = section_1_bank.text_input(label="재이전계좌 은행",value=None,label_visibility="collapsed")
             section_1_acountIndex.write("계좌 번호 : ")
-            section_1_acount.number_input(label="재이전계좌 번호",value=None,step=1,label_visibility="collapsed")
+            acount1 = section_1_acount.number_input(label="재이전계좌 번호",value=None,step=1,label_visibility="collapsed")
             section_1_nameIndex.write("명의인 : ")
-            section_1_name.text_input(label="재이전계좌 명의인",value=None,label_visibility="collapsed")
+            name1 = section_1_name.text_input(label="재이전계좌 명의인",value=None,label_visibility="collapsed")
         else:
             section_1_bankIndex.write("은행: ")
-            section_1_bank.write(mid)
+            bank1 = section_1_bank.write(mid)
             section_1_acountIndex.write("계좌 번호: ")
-            section_1_acount.write(mid)
+            acount1 = section_1_acount.write(mid)
             section_1_nameIndex.write("명의인 : ")
-            section_1_name.write(mid)
-        st.write("2.입금 모계좌")
-        section_2_bankIndex,section_2_bank,section_2_acountIndex,section_2_acount = st.columns(spec=[1,1,1,3],vertical_alignment="center")
+            name1 = section_1_name.write(mid)
+        st.write("### 2.입금 모계좌")
+        section_2_bankIndex,section_2_bank,section_2_acountIndex,section_2_acount = st.columns(spec=[1,1,1,3],gap="small",vertical_alignment="center")
         section_2_bankIndex.write("은행 : ")
-        section_2_bank.text_input(label="입금계좌 은행",value=None,label_visibility="collapsed")
+        bank2 = section_2_bank.text_input(label="입금계좌 은행",value=None,label_visibility="collapsed")
         section_2_acountIndex.write("계좌 번호 : ")
-        section_2_acount.number_input(label="입금계좌 번호",value=None,step=1,label_visibility="collapsed")
-        st.write("3.출금(정산) 모계좌")
-        section_3_bankIndex,section_3_bank,section_3_acountIndex,section_3_acount = st.columns(spec=[1,1,1,3],vertical_alignment="center")
-        section_3_bankIndex.write("은행 : ")
-        section_3_bank.write(mid)
-        section_3_acountIndex.write("계좌 번호 : ")
-        section_3_acount.write(mid)
-        st.write("4.피해정보")
-        section_2_timeIndex,section_2_day,section_2_time,empty,empty = st.columns(spec=[1,1,1,1,1],vertical_alignment="center")
-        section_1_timeIndex,section_1_day,section_1_time,empty,empty = st.columns(spec=[1,1,1,1,1],vertical_alignment="center")
-        section_1_costIndex,section_1_cost,empty,empty = st.columns(spec=[1,2,1,1],vertical_alignment="center")
+        acount2 = section_2_acount.number_input(label="입금계좌 번호",value=None,step=1,label_visibility="collapsed")
+        st.write("### 3.피해정보")
+        section_2_timeIndex,section_2_day,section_2_time,empty,empty = st.columns(spec=[1,1,1,1,1],gap="small",vertical_alignment="center")
+        section_1_timeIndex,section_1_day,section_1_time,empty,empty = st.columns(spec=[1,1,1,1,1],gap="small",vertical_alignment="center")
+        section_1_costIndex,section_1_cost,empty,empty = st.columns(spec=[1,2,1,1],gap="small",vertical_alignment="center")
         section_2_timeIndex.write("입금 시간 : ")
-        section_2_day.date_input(label="입금 날짜",label_visibility="collapsed")
-        section_2_time.text_input(label="입금 시간",value=None,label_visibility="collapsed")
+        day2 = section_2_day.date_input(label="입금 날짜",label_visibility="collapsed")
+        time2 = section_2_time.text_input(label="입금 시간",value=None,label_visibility="collapsed")
         section_1_timeIndex.write("정산 시간 : ")
-        section_1_day.date_input(label="정산 날짜",label_visibility="collapsed")
-        section_1_time.text_input(label="정산 시간",value=None,label_visibility="collapsed")
+        day1 = section_1_day.date_input(label="정산 날짜",label_visibility="collapsed")
+        time1 = section_1_time.text_input(label="정산 시간",value=None,label_visibility="collapsed")
         section_1_costIndex.write("피해금 : ")
-        section_1_cost.number_input(label="피해금",value=None,step=1,label_visibility="collapsed")
-    #html_form 불러오기
-    with open(htmlForm.format(servise="선불"),"r",encoding="UTF-8") as html:
-        html = html.read()
-    #jinja templete 변경 및 formating
-    fax = Template(html).render(
-        section_1_bank = "", #재이전 모계좌 은행
-        section_1_acount = "", #재이전 모계좌 번호
-        section_1_name = "", #재이전 가맹점
-        section_1_time = "", #정산 시간
-        section_1_cost = "", #피해금
-        section_2_bank = "", #입금 은행
-        section_2_acount = "", #입금 계좌
-        section_2_time = "", #입금 시간
-        section_3_bank = "", #정산 계좌 은행
-        section_3_acount = "", #정산 계좌 번호
-        trNum = "", #거래번호
-        orNum = "", #주문번호
-        otherInfomation = "", #특이사항
-        sendBank = "", #수신 은행
-        today = "" #발신 날짜
-    )
+        cost1 = section_1_cost.number_input(label="피해금",value=None,step=1,label_visibility="collapsed")
+        st.write("### 4.기타정보")
+        trNumIndex,trNum,empty = st.columns(spec=[1,3,1],gap="small",vertical_alignment="center")
+        trNumIndex.write("거래번호 : ")
+        tradeNo = trNum.text_input(label="거래번호",value=None,label_visibility="collapsed")
+        orNumIndex,orNum,empty = st.columns(spec=[1,3,1],gap="small",vertical_alignment="center")
+        orNumIndex.write("주문번호 : ")
+        orderNo = orNum.text_input(label="주문번호",value=None,label_visibility="collapsed")
+        otherInfomationIndex,otherInfomation,empty = st.columns(spec=[1,3,1],gap="small",vertical_alignment="top")
+        otherInfomationIndex.write("기타사항 : ")
+        antherInfo = otherInfomation.text_area(label="주문번호",value=None,label_visibility="collapsed")
+        sendbankIndex,sendbank,empty = st.columns(spec=[1,3,1],gap="small",vertical_alignment="center")
+        sendbankIndex.write("발송은행 : ")
+        sendbank = sendbank.text_input(label="발송은행",value=None,label_visibility="collapsed")
+        empty,savebtn = st.columns(spec=[4,1],gap="small",vertical_alignment="center")
+        with open(htmlForm.format(servise="선불"),"r",encoding="UTF-8") as html:
+            html = html.read()
+        results = formating(form=html,
+                            sec_1_bank=bank1,
+                            sec_1_acount=acount1,
+                            sec_1_name=name1,
+                            sec_1_time=f"{day1}<br>{time1}",
+                            sec_1_cost=cost1,
+                            sec_2_bank=bank2,
+                            sec_2_acount=acount2,
+                            sec_2_time=f"{day2}<br>{time2}",
+                            sec_3_bank="fixed",
+                            sec_3_acount="fixed",
+                            tradeNo=tradeNo,
+                            orderNo=orderNo,
+                            antherInfo=antherInfo,
+                            send=sendbank
+                            )
+        if savebtn.button("저장"):
+            htmlOutput = f"C:\\Users\\USER\\ve_1\\samplePage\\fax8html\\{day2}_{sendbank}_{cost1}.html"
+            imgOutput = f"C:\\Users\\USER\\ve_1\\samplePage\\fax8image\\{day2}_{sendbank}_{cost1}.png"
+            with open(htmlOutput,"w",encoding="UTF-8") as html:
+                html.write(results)
+            toImage(htmlOutput,imgOutput)
